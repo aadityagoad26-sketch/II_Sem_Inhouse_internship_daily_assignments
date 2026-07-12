bbb54bcc7e1bc89cf82aa25e2b9dc60211022364
Aditya Sharma <aadityagoad26@gmail.com>
2026-07-12 22:48:42 +0530

    Add files via upload

---
+++ 25ESKCY004/Day 7/registration.php
@@ -0,0 +1,10 @@
+<?php
+
+$name = $_GET["name"];
+$branch = $_GET["branch"];
+$email = $_GET["email"];
+$phonenumber = $_GET["phonenumber"];
+echo"values received : $name $branch $email $phonenumber";
+?>
+
+
